79346cfadc55c18fc284827f30e89541e475dd33
wagnerlm <wagnerlink@gmail.com>
2024-03-21 14:15:53 -0300

    enable tls

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_db_proxy" "rds_proxy" {
   debug_logging           = true
   engine_family           = "POSTGRESQL"
   idle_client_timeout     = 1800
-  require_tls             = false
+  require_tls             = true
   role_arn                = aws_iam_role.rds_proxy_role.arn
   vpc_security_group_ids  = var.vpc_security_group_ids
   vpc_subnet_ids          = var.subnet_ids
